7adebb1d5bac39e333506595260f38997c6ab634
saeide <82383927+saeide1997@users.noreply.github.com>
2024-09-17 09:29:22 +0330

    Add files via upload

---
+++ NodeJs/models/User.js
@@ -0,0 +1,12 @@
+const mongoose =  require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+    userName: { type: String, require: true, unique: true},
+    email: { type: String, require: true, unique: true},
+    password: { type: String, require: true, unique: true},
+    isAdmin: { type: Boolean, default: false},
+    
+},
+{timestamps: true})
+
+module.exports = mongoose.model("User", UserSchema)
